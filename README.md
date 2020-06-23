b4bb90271849beeb89075dc29b4f4a06d0b5e72d
Riccardo Giorato <giorat.96@gmail.com>
2020-06-23 08:16:14 +0200

    setup tailwind and remove starter code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # newtab
 minimal newtab without ads or "Paid" features
+
+I need this new tab to be:
+- minimal no signups or any other stuff or pro thing
+- not use battery if possible
+- showcase current time
+- showcase my next calendar event
+- provide links to the right tool at the right time based on my routine
+  - in the morning - trello
+  - after work - workout or running/walking
+  - after dinner - trello
+  - after work dinner - calendar to log my day
+- 
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="bg-gray-100 py-16 w-screen h-screen ">
+        <div className="container px-4 sm:px-8 lg:px-16 xl:px-20 mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-8 items-center">
+            <div className="col-span-6">
+              <h1 className=" font-bold text-4xl md:text-5xl max-w-xl text-gray-900 leading-tight">
+                08:11
+              </h1>
+              <h1 className=" font-bold text-3xl max-w-xl text-gray-900 leading-tight">
+                Martedì, 22 Giugno
+              </h1>
+              <p className="text-gray-800 text-base leading-relaxed mt-8 font-semibold">
+                calendar
+              </p>
+            </div>
+
+            <div className="col-span-6">
+              <h1 className=" font-bold text-3xl max-w-xl text-gray-900 leading-tight">
+                right column
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
